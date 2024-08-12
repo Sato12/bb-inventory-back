bce452d9bc250be4b89f03b7f6fcebfb15b1b4f4
Santiago <gonzalezsae@globalhitss.com>
2024-08-12 06:21:32 -0500

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,12 @@ import { PORT } from './config/server.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+  });
   app.useGlobalPipes(new ValidationPipe({ forbidUnknownValues: true, forbidNonWhitelisted: true, whitelist: true }));
 
   await app.listen(PORT);
 }
-console.log('XXXXXXXX');
-console.log(process.env.MONGO);
-console.log('XXXXXXXX');
+
 bootstrap();
